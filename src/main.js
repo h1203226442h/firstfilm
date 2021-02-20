b6c3bf31ed1cf358116bb49644800e79c71fd32e
h1203226442h <1203226442@qq.com>
2021-02-20 12:13:10 +0800

    add getCity

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import axios from 'axios'
 
 Vue.prototype.axios = axios //其他组件可通过this.axios引用
 
+//全局注册 滑动组件
+import Scroller from '@/components/Scroller' 
+Vue.component('Scroller',Scroller)
+
 Vue.config.productionTip = false
 
 new Vue({
